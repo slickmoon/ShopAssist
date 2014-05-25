a50ad6cf1442a281488d6000af941611bdba5726
jakerenzella <jakerenzella94@gmail.com>
2014-05-25 14:32:16 +1000

    Created item, itemList
    Added item and itemList classes, had to deviate from class diagram in
    changeQty.

---
+++ ShopAssist/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShopAssist
+{
+    class Item
+    {
+        private String itemID;
+        private String name;
+        private int qty;
+
+        public Item()
+        {
+
+        }
+        public void changeQuantity(int qtyDelta)
+        {
+            qty = qty + qtyDelta;
+        }
+    }
+}
---
+++ ShopAssist/itemList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShopAssist
+{
+    class itemList
+    {
+        private List<Item> itemList = new List<Item>();
+
+        public void addItem(Item item)
+        {
+            if (!itemList.Contains(item))
+            {
+                itemList.Add(item);
+            }
+            else
+            {
+                item.changeQuantity(1);   
+            }
+        }
+
+        public void removeItem(Item item)
+        {
+            itemList.Remove(item);
+        }
+    }
+}
