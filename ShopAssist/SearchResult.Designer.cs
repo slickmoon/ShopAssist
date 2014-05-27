6a3b29a3b965984778bb70fa919dd4c956e8423f
Nick <nickcass88@gmail.com>
2014-05-28 00:11:30 +1000

    Search Screen Added
    -Search Screen gui added
    -Small bug fixes

---
+++ ShopAssist/SearchResult.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace ShopAssist
+{
+    partial class SearchResult
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblHeading = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.btnSortPrice = new System.Windows.Forms.Button();
+            this.btnSortStore = new System.Windows.Forms.Button();
+            this.lblSort = new System.Windows.Forms.Label();
+            this.btnReturn = new System.Windows.Forms.Button();
+            this.btnViewSelected = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 116);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(346, 312);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Search Results List:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(359, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "To view an item, click on the desired item, followed by \'View Item\'";
+            // 
+            // lblHeading
+            // 
+            this.lblHeading.AutoSize = true;
+            this.lblHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeading.Location = new System.Drawing.Point(18, 18);
+            this.lblHeading.Name = "lblHeading";
+            this.lblHeading.Size = new System.Drawing.Size(338, 29);
+            this.lblHeading.TabIndex = 3;
+            this.lblHeading.Text = "Search Results for \"undefined\"";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(388, 116);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(69, 312);
+            this.textBox2.TabIndex = 4;
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrice.Location = new System.Drawing.Point(388, 97);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(38, 15);
+            this.lblPrice.TabIndex = 5;
+            this.lblPrice.Text = "Price:";
+            // 
+            // btnSortPrice
+            // 
+            this.btnSortPrice.Location = new System.Drawing.Point(74, 435);
+            this.btnSortPrice.Name = "btnSortPrice";
+            this.btnSortPrice.Size = new System.Drawing.Size(75, 23);
+            this.btnSortPrice.TabIndex = 6;
+            this.btnSortPrice.Text = "Price";
+            this.btnSortPrice.UseVisualStyleBackColor = true;
+            // 
+            // btnSortStore
+            // 
+            this.btnSortStore.Location = new System.Drawing.Point(167, 435);
+            this.btnSortStore.Name = "btnSortStore";
+            this.btnSortStore.Size = new System.Drawing.Size(75, 23);
+            this.btnSortStore.TabIndex = 7;
+            this.btnSortStore.Text = "Store Name";
+            this.btnSortStore.UseVisualStyleBackColor = true;
+            // 
+            // lblSort
+            // 
+            this.lblSort.AutoSize = true;
+            this.lblSort.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSort.Location = new System.Drawing.Point(20, 438);
+            this.lblSort.Name = "lblSort";
+            this.lblSort.Size = new System.Drawing.Size(48, 15);
+            this.lblSort.TabIndex = 8;
+            this.lblSort.Text = "Sort By:";
+            // 
+            // btnReturn
+            // 
+            this.btnReturn.Location = new System.Drawing.Point(21, 474);
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(112, 43);
+            this.btnReturn.TabIndex = 9;
+            this.btnReturn.Text = "Return Home";
+            this.btnReturn.UseVisualStyleBackColor = true;
+            // 
+            // btnViewSelected
+            // 
+            this.btnViewSelected.Location = new System.Drawing.Point(255, 474);
+            this.btnViewSelected.Name = "btnViewSelected";
+            this.btnViewSelected.Size = new System.Drawing.Size(112, 43);
+            this.btnViewSelected.TabIndex = 10;
+            this.btnViewSelected.Text = "View Selected";
+            this.btnViewSelected.UseVisualStyleBackColor = true;
+            // 
+            // SearchResult
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 538);
+            this.Controls.Add(this.btnViewSelected);
+            this.Controls.Add(this.btnReturn);
+            this.Controls.Add(this.lblSort);
+            this.Controls.Add(this.btnSortStore);
+            this.Controls.Add(this.btnSortPrice);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblHeading);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "SearchResult";
+            this.Text = "ShopAssist - Search Results";
+            this.Load += new System.EventHandler(this.SearchResult_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblHeading;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Button btnSortPrice;
+        private System.Windows.Forms.Button btnSortStore;
+        private System.Windows.Forms.Label lblSort;
+        private System.Windows.Forms.Button btnReturn;
+        private System.Windows.Forms.Button btnViewSelected;
+    }
+}
---
+++ ShopAssist/SearchResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ShopAssist
+{
+    public partial class SearchResult : Form
+    {
+        public SearchResult()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchResult_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
